2dbf28d91e57892531ee9e7a670721bcb46be0c0
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-07-10 18:56:34 +0300

    Add linking table  - BookGanre
    A limking table has been added between  books and ganre

---
+++ Booksy.Data.Models/BookGanre.cs
@@ -0,0 +1,11 @@
+﻿namespace Booksy.Data.Models
+{
+    public class BookGanre
+    {
+        public Guid BookId { get; set; }
+        public Book Book { get; set; } = null!;
+
+        public Guid GanreId { get; set; }
+        public Ganre Ganre { get; set; } = null!;
+    }
+}
